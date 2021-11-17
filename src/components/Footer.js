0082915ae4deb62c5e43525a57b90d7be074ed6b
K P Balaji Reddy <kpbalaji031@gmail.com>
2021-11-17 20:30:01 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer =()=> {
+   
+        return (
+            <div className="">
+                <footer _ngcontent-serverapp-c52="" class="container-fluid bg-dark my-0 py-3 text-light"><p _ngcontent-serverapp-c52="" class="mb-0 text-center">Copyright © 2020-2021 NewsHighlights.ga</p></footer>
+            </div>
+        )
+    
+}
+
+export default Footer
